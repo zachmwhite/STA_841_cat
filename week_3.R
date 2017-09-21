3459c1b7240f317b594cb418fa3418ab6e4bfc10
zachmwhite <zachary.m.white@duke.edu>
2017-09-21 14:02:01 -0400

    HW 1 HW 1 Solutions HW 2 Initial

--- week_3.R
+++ week_3.R
@@ -10,3 +10,10 @@ prest.lm = lm(log.pubs ~ phdprestige,data = PhdPubs)
 summary(prest.lm)
 
 library(js)
+library(animation)
+
+newton.method(FUN= function(p) choose(10,5)p^5(1-p)^5, )
+
+newton.method(FUN = function(p) 5 /p - (5 /(1-p)),init = .8,rg = c(.2,.8),interact = TRUE)
+
+newton.method(FUN = function(p) 5 /p - (5 /(1-p)),init = .8,rg = c(.1,.9),interact = TRUE)
